06c65b48f2b2c78423985ad1edfb31f4824c61af
Randy Goss <randycgoss@gmail.com>
2018-12-20 19:43:16 -0800

    Added destroy for brands

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,5 +1,13 @@
 class BrandsController < ApplicationController
-	def create
+
+	def destroy
+		@bar = Bar.find(params[:bar_id])
+		@brand = Brand.find(params[:id])
+		@brand.destroy
+		redirect_to bar_path(@bar)
+	end
+
+  def create
     @bar = Bar.find(params[:bar_id])
     @bar.brands.create(brand_params)
     redirect_to bar_path(@bar)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 	root 'userlocations#search'
 	resources :userlocations, only: :create
 	resources :bars do
-		resources :brands, only: :create
+		resources :brands, only: [:create, :destroy]
 		resources :photos, only: :create
 	end
 	get '/results', to: 'bars#results'
