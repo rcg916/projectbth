eec7159f27102601af5b79d23d0191fe0f3ddb7e
Randy Goss <randycgoss@gmail.com>
2018-12-20 20:52:02 -0800

    Made a few small changes to styling and added help page content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	def favoritebar_text
-		return @favoritebar_exists ? "UnFavorite" : "Favorite"
+		return @favoritebar_exists ? "Remove from Favorites" : "Add to Favorites"
 	end
 
 	helper_method :favoritebar_text
